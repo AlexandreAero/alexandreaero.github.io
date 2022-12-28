9c64a4f9c7967cf286f9d3f8c9d3a74ed2a9846c
Pilot Alex <gumery.alexandre@gmail.com>
2022-12-28 16:01:38 +0100

    Removed hardcoded table

--- post.js
+++ post.js
@@ -1,52 +1,52 @@
 const converter = new showdown.Converter();
 
-const table = `
-|key|value|
-|-----------|-----------|
-|title|That time my Discord server has been raided because of a dumb mistake.|
-|author|By Pilot Alex|
-|date|24/09/2022|
-|description|I used to have a Discord server a little while ago with my friends, it was a private server with only 4-5 people in it. But things started to get messy when I developed a Discord bot that leaked our server secret key.|
-|tags|Discord;Server;Bot;JavaScript|
-|thumbnail|https://avatars.githubusercontent.com/u/26492485?s=200&v=4|
-`;
-
 fetch(sessionStorage.getItem('page'))
 .then((res) => res.text())
 .then((out) => {
-    document.getElementById("title").innerHTML = getTableValue(table, "title");
-    document.getElementById("author").innerHTML = getTableValue(table, "author");
-    document.getElementById("date").innerHTML = getTableValue(table, "date");
-    document.getElementById("description").innerHTML = getTableValue(table, "description");
+  const table = extractTable(out);
 
-    let contentWrapper = document.getElementById("main-content-wrapper");
-    let html = converter.makeHtml(out);
+  document.getElementById('title').innerHTML = getTableValue(table, 'title');
+  document.getElementById('author').innerHTML = getTableValue(table, 'author');
+  document.getElementById('date').innerHTML = getTableValue(table, 'date');
+  document.getElementById('description').innerHTML = getTableValue(table, 'description');
 
-    contentWrapper.insertAdjacentHTML("beforeend", html);
+  let contentWrapper = document.getElementById('main-content-wrapper');
+  let html = converter.makeHtml(out);
 
-    hljs.highlightAll();
+  contentWrapper.insertAdjacentHTML('beforeend', html);
+
+  hljs.highlightAll();
 });
 
 /**
  * 
- * @param {string} key 
+ * @param {string} out
+ * @returns 
+ */
+function extractTable(out) {
+  return out.split('*')[0].trim();
+}
+
+/**
+ * 
+ * @param {string} key
  * @returns 
  */
 function getTableValue(table, key) {
-    let result = {};
+  let result = {};
   
-    table = table.trim();
+  table = table.trim();
   
-    let split = table.split("\n");
+  let split = table.split('\n');
   
-    for(let i = 2; i < split.length; i++) {
-      let temp = split[i];
+  for(let i = 2; i < split.length; i++) {
+    let temp = split[i];
       
-      const key = temp.split("|")[1].trim();
-      const value = temp.split("|")[2].trim();
+    const key = temp.split('|')[1].trim();
+    const value = temp.split('|')[2].trim();
   
-      result[key] = value;
-    }
+    result[key] = value;
+  }
   
-    return result[key];
+  return result[key];
 }
